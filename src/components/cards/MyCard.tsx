4a05db1e6a155b401658cac3601f8f844a297321
BenK93 <benkoren23@gmail.com>
2021-09-14 20:30:40 +0300

    Improving Game

--- src/components/cards/MyCard.tsx
+++ src/components/cards/MyCard.tsx
@@ -1,11 +1,10 @@
-import Card from '@material-ui/core/Card';
-import './MyCard.css'
-import Button from '@material-ui/core/Button';
-import SentimentVerySatisfiedTwoToneIcon from '@material-ui/icons/SentimentVerySatisfiedTwoTone';
-import SentimentVeryDissatisfiedTwoToneIcon from '@material-ui/icons/SentimentVeryDissatisfiedTwoTone';
-import Typography from '@material-ui/core/Typography';
-import { Zoom } from '@material-ui/core';
-
+import Card from "@material-ui/core/Card";
+import "./MyCard.css";
+import Button from "@material-ui/core/Button";
+import SentimentVerySatisfiedTwoToneIcon from "@material-ui/icons/SentimentVerySatisfiedTwoTone";
+import SentimentVeryDissatisfiedTwoToneIcon from "@material-ui/icons/SentimentVeryDissatisfiedTwoTone";
+import Typography from "@material-ui/core/Typography";
+import { Zoom } from "@material-ui/core";
 
 interface CardProps {
   guessWord: String;
@@ -16,7 +15,7 @@ interface CardProps {
 }
 
 const MyCard = (props: CardProps) => {
-  const bull = <span >•</span>;
+  const bull = <span>•</span>;
 
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
   return (
@@ -24,9 +23,9 @@ const MyCard = (props: CardProps) => {
       in={props.showCard}
       timeout={500}
       style={{
-        margin: '20px 30px 20px 30px',
-        borderRadius: '20px',
-        width: '100%'
+        margin: "0px 30px 20px 30px",
+        borderRadius: "20px",
+        width: "100%",
       }}
       unmountOnExit
     >
@@ -37,29 +36,40 @@ const MyCard = (props: CardProps) => {
           </Typography>
         </div>
         <div>
-          {props.words.map(word => (
-            <p className="word" >
-              <strong> {word} {bull} <br /></strong>
+          {props.words.map((word) => (
+            <p className="word">
+              <strong>
+                {" "}
+                {word} {bull} <br />
+              </strong>
             </p>
           ))}
         </div>
         <div className="card-btn-con">
           <Button
             onClick={() => props.onSkip()}
-            className="card-btn" variant="contained" style={{ borderRadius: '25px', backgroundColor: '#FF6347' }} size="medium">
+            className="card-btn"
+            variant="contained"
+            style={{ borderRadius: "25px", backgroundColor: "#FF6347" }}
+            size="medium"
+          >
             <SentimentVeryDissatisfiedTwoToneIcon />
             <span className="button-text">דלג</span>
           </Button>
           <Button
             onClick={() => props.onNext()}
-            className="card-btn" variant="contained" style={{ borderRadius: '25px', backgroundColor: "#46da46" }} size="medium">
+            className="card-btn"
+            variant="contained"
+            style={{ borderRadius: "25px", backgroundColor: "#46da46" }}
+            size="medium"
+          >
             <SentimentVerySatisfiedTwoToneIcon />
             <span className="button-text">הבא</span>
           </Button>
         </div>
       </Card>
     </Zoom>
-  )
-}
+  );
+};
 
-export default MyCard
+export default MyCard;
